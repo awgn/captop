c904ceff44eae01d5993b7ff94b0f9aa20f825be
Nicola Bonelli <nicola.bonelli@cnit.it>
2014-12-08 12:05:01 +0100

    Failure is when pcap_inject return -1

--- src/captop.cpp
+++ src/captop.cpp
@@ -242,10 +242,7 @@ packet_handler(u_char *randgen, const struct pcap_pkthdr *h, const u_char *paylo
         }
 
         int ret = pcap_inject(global::out, payload, h->caplen);
-        if (ret == -1)
-            throw std::runtime_error("pcap_inject:" + std::string(pcap_geterr(global::out)));
-
-        if (ret > 0)
+        if (ret >= 0)
         {
             global::out_count.fetch_add(1, std::memory_order_relaxed);
             global::out_band.fetch_add(h->len, std::memory_order_relaxed);
