ae1bf7acd589192bdb8664ce52b34bb6eac15471
Nicola Bonelli <nicola.bonelli@gmail.com>
2017-04-12 12:53:32 +0200

    captop.h header updated.

--- hdr/captop.h
+++ hdr/captop.h
@@ -2,7 +2,13 @@
 
 #include <pcap/pcap.h>
 
+#ifdef __cplusplus
 extern "C"
 {
+#endif
+
 	void captop_handler(u_char *, const struct pcap_pkthdr *h, const u_char *payload);
+
+#ifdef __cplusplus
 }
+#endif
